1d2d4261ee1aad99949d9ea4997d165f312eeea3
Biancadmsa Salcedo <biancadmsadf16@gmail.com>
2023-09-11 21:29:12 +0000

    miprimercommit

--- .learn/resets/01-hello-world/app.jsx
+++ .learn/resets/01-hello-world/app.jsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 // WHAT: This variable returns contains the html to render
 let output = <span>James is 12 years old</span>;
 
-// WHERE: A DOM element that  will contain the entire react generated html
+// WHERE: A DOM element that will contain the entire react generated html
 const myDiv = document.querySelector("#myDiv");
 
 //what   //where
